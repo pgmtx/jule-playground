58b801631e60e79b15627b216648d86a8277d923
pgmtx <programutox@disroot.org>
2026-02-10 22:29:22 +0100

    Remake current interface using CodeMirror

--- playground.js
+++ playground.js
@@ -1,17 +1,27 @@
+import { EditorView, basicSetup } from "https://esm.sh/codemirror@6.0.1";
+import { javascript } from "https://esm.sh/@codemirror/lang-javascript@6.2.1";
+
 function main() {
-    const codearea = document.getElementById("input-code");
-    codearea.value = 'fn main() {\n    println("Hello world!")\n}\n';
+  const editor = new EditorView({
+    doc: `function hello() {
+  console.log("Hello World!");
+  return true;
+}`,
+    extensions: [basicSetup, javascript()],
+    parent: document.getElementById("editor"),
+  });
 
-    const runButton = document.getElementById("run-button");
-    runButton.onclick = () => {
-        alert("you clicked run button");
-    };
+  const runButton = document.getElementById("run-button");
+  runButton.onclick = () => {
+    console.log("Run button clicked!");
+    console.log("Code:\n", editor.state.doc.toString());
+  };
 
-    document.addEventListener("keydown", (e) => {
-      if (e.ctrlKey && e.key === "Enter") {
-        runButton.click();
-      }
-    })
+  document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key === "Enter") {
+      runButton.click();
+    }
+  });
 }
 
 main();

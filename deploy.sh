1080cfcbd9bcb473f3edba71b05de5ed807b2aff
pgmtx <programutox@disroot.org>
2026-02-14 21:43:06 +0100

    Fix filenames in the deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,9 +6,9 @@ if [ ! -f ./.env ]; then
 fi
 
 source .env
-CGO_ENABLED=0 go build -o main -ldflags "-s -w" .
+CGO_ENABLED=0 go build -ldflags "-s -w" .
 ssh root@$SERVER_IP "systemctl stop playground"
-scp ./main root@$SERVER_IP:/opt/playground/
+scp ./playground root@$SERVER_IP:/opt/playground/
 scp -r ./public root@$SERVER_IP:/opt/playground/
 ssh root@$SERVER_IP "systemctl start playground"
 echo "Code deployed successfully!"

69b86f59588b955d5a2c3294babc208823f2dcd7
pgmtx <programutox@disroot.org>
2026-02-14 15:29:36 +0100

    Improve and refactor command executions and error handling

--- main.go
+++ main.go
@@ -8,7 +8,37 @@ import (
 	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 )
+
+var ansi *regexp.Regexp = regexp.MustCompile(`\x1b\[[0-9;]*m`)
+
+func executeCommand(w http.ResponseWriter, command string, args ...string) (string, bool) {
+	build := exec.Command(command, args...)
+	output, err := build.CombinedOutput()
+
+	// Jule error messages use ANSI color codes, so they must be filtered out for
+	// web display.
+	outputStr := ansi.ReplaceAllString(string(output), "")
+	outputStr = strings.TrimSpace(outputStr)
+	// For some reason in some error messages there is a null character
+	// which is displayed as a square.
+	outputStr = strings.ReplaceAll(outputStr, "\x00", "")
+
+	if err == nil {
+		return outputStr, true
+	}
+
+	if outputStr != "" {
+		fmt.Println(outputStr)
+		http.Error(w, outputStr, 500)
+	} else {
+		fmt.Println("Error: ", err.Error())
+		http.Error(w, err.Error(), 500)
+	}
+
+	return outputStr, false
+}
 
 func postHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
@@ -19,25 +49,21 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	codeInput, _ := io.ReadAll(r.Body)
 	os.WriteFile("main.jule", codeInput, 0644)
 
-	cmd := exec.Command(
-		"docker", "run", "--rm",
-		"-v", ".:/work",
-		"-w", "/work",
-		"jule-clang",
-		"sh", "-c", "/jule/bin/julec build . && ./main",
-	)
+	julecPath := os.Getenv("JULEC_PATH")
+	if julecPath == "" {
+		julecPath = "julec"
+	}
 
-	ansi := regexp.MustCompile(`\x1b\[[0-9;]*m`)
-	output, err := cmd.CombinedOutput()
-	if err != nil {
-		http.Error(w, fmt.Sprintf("Error: %v\n%s", err, output), 500)
+	const programName = "program"
+	_, ok := executeCommand(w, julecPath, "build", "-o", programName, ".")
+	if !ok {
 		return
 	}
 
-	// Jule error messages use ANSI color codes, so they must be filtered out for
-	// web display.
-	outputStr := ansi.ReplaceAllString(string(output), "")
-	w.Write([]byte(outputStr))
+	outputStr, ok := executeCommand(w, "./"+programName)
+	if ok {
+		fmt.Fprint(w, outputStr)
+	}
 }
 
 func main() {
